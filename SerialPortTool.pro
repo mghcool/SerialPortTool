4247f3dbc4784d12df364b8cb6da337d5e5c1baa
mgh <mghcool@163.com>
2021-07-04 18:26:23 +0800

    添加基础布局

--- SerialPortTool.pro
+++ SerialPortTool.pro
@@ -1,4 +1,5 @@
 QT       += core gui
+QT       += serialport
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
@@ -25,7 +26,13 @@ HEADERS += \
 FORMS += \
     mainwindow.ui
 
+
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+RESOURCES += \
+    Resources.qrc
+
+RC_ICONS = icon.ico
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,22 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QSerialPort>        //提供访问串口的功能
+#include <QSerialPortInfo>    //提供系统中存在的串口的信息
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //setWindowIcon(QIcon("icon.ico"));
+    QLabel *statusLabel;
+    statusLabel = new QLabel;
+    statusLabel->setMinimumSize(150, 20); // 设置标签最小大小
+    //statusLabel->setFrameShape(QFrame::WinPanel); // 设置标签形状
+    statusLabel->setFrameShadow(QFrame::Sunken); // 设置标签阴影
+    ui->statusbar->addWidget(statusLabel);
+    statusLabel->setText(tr("串口调试助手准备就绪！"));
 }
 
 MainWindow::~MainWindow()
